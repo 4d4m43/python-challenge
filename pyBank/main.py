49126e3c5acb0a2e8ee8a11c8e3f0325f61ac9ab
4d4m43 <adam.abdelmalek@gmail.com>
2018-12-01 14:42:03 -0600

    added budget_dat.csv, and initiated main.py for PyBank

--- pyBank/main.py
+++ pyBank/main.py
@@ -1,1 +1,19 @@
+# PyBank main.py
+# author @ Adam Abdelmalek
+# date @ 120118
 
+import os
+import csv
+
+bank_csv = os.path.join("C:/Users/adama/Desktop/3/Activities/01-Stu_CerealCleaner/Resources", "cereal_bonus.csv")
+
+with open(bank_csv, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    #print(csvreader)
+    
+    #csv_header = next(csvreader)
+    #print(f"{csv_header}")
+      
+    for row in csvreader:
+        if float(row[7]) >= 5: 
+            print(row)  
