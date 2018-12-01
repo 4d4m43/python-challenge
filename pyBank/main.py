7215d8ce3a9fa2300ba7b34c9328bc3fae2baa16
4d4m43 <adam.abdelmalek@gmail.com>
2018-12-01 15:42:36 -0600

    added budget_dat.csv, and initiated main.py for PyBank

--- pyBank/main.py
+++ pyBank/main.py
@@ -5,15 +5,73 @@
 import os
 import csv
 
-bank_csv = os.path.join("C:/Users/adama/Desktop/3/Activities/01-Stu_CerealCleaner/Resources", "cereal_bonus.csv")
+bank_csv = os.path.join("C:/Users/adama/Desktop/python-challenge/PyBank/Resources", "budget_data.csv")
 
 with open(bank_csv, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #print(csvreader)
     
-    #csv_header = next(csvreader)
+    csv_header = next(csvreader)
     #print(f"{csv_header}")
-      
+    
+    #Calculated Values monthCounter, dollarTotal, avgChange, greatestInc, greatestDec
+    monthCounter = 0
+    dollarTotal = 0
+    avgChange = 0
+    greatestInc = 0
+    greatestDec = 0
+    prevDollar = 0
+    prevDelta = 0
+    deltaTotal = 0
+    delta = 0
+
     for row in csvreader:
-        if float(row[7]) >= 5: 
-            print(row)  
+
+        dollar = float(row[1])
+        dollarTotal =  round(float(dollarTotal) + dollar,2)
+
+        print(dollarTotal)
+        if monthCounter >= 1:
+            delta = dollar - prevDollar
+            deltaTotal = deltaTotal + delta
+        
+        monthCounter = monthCounter + 1
+
+        if delta > greatestInc:
+            greatestInc = delta
+            greatestIncDate = row[0]
+        elif delta < greatestDec:
+            greatestDec = delta
+            greatestDecDate = row[0]
+
+        prevDelta = delta
+        prevDollar = dollar
+
+avgChange = round(deltaTotal/(monthCounter-1),2)
+
+print("Financial Analysis")
+print(" ----------------------------")
+print(f"Total Months    :   {monthCounter}")
+print(f"Total           :   ${dollarTotal}")
+print(f"Average Change  :   ${avgChange}")
+print(f"Greatest Increase in Profits    :   {greatestIncDate} ${greatestInc}")
+print(f"Greatest Decrease in Profits    :   {greatestDecDate} ${greatestDec}")
+
+
+with open("Output.txt", "w") as text_file:
+    print("Financial Analysis", file=text_file)
+    print(" ----------------------------", file=text_file)
+    print(f"Total Months    :   {monthCounter}", file=text_file)
+    print(f"Total           :   ${dollarTotal}", file=text_file)
+    print(f"Average Change  :   ${avgChange}", file=text_file)
+    print(f"Greatest Increase in Profits    :   {greatestIncDate} ${greatestInc}", file=text_file)
+    print(f"Greatest Decrease in Profits    :   {greatestDecDate} ${greatestDec}", file=text_file)
+
+ #Financial Analysis
+ # ----------------------------
+ # Total Months: 86
+ # Total: $38382578
+ # Average  Change: $-2315.12
+ # Greatest Increase in Profits: Feb-2012 ($1926159)
+ # Greatest Decrease in Profits: Sep-2013 ($-2196167)
+
